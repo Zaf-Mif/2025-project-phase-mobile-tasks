e1a23998fe2697dc7aeefed93cb8bb520b86eab4
Zaferan Miftah <zaferanmiftah@gmail.com>
2025-07-31 15:21:54 +0300

    make a clean architecture

---
+++ ecommerce_app/test/features/product/data/model/product_model_test.dart
@@ -0,0 +1,37 @@
+import 'package:ecommerce_app/features/product/data/models/product_model.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  late ProductModel productModel;
+  late Map<String, dynamic> json;
+
+  setUp(() {
+    productModel = const ProductModel(
+      id: 1,
+      name: 'Test Product',
+      price: 49.99,
+      imageUrl: 'test.jpg',
+      description: 'Test description',
+    );
+
+    json = {
+      'id': 1,
+      'name': 'Test Product',
+      'price': 49.99,
+      'image': 'test.jpg', // Mapped from imageUrl
+      'description': 'Test description',
+    };
+  });
+
+  group('ProductModel', () {
+    test('fromJson should return valid ProductModel', () {
+      final result = ProductModel.fromJson(json);
+      expect(result, equals(productModel));
+    });
+
+    test('toJson should return a valid map', () {
+      final result = productModel.toJson();
+      expect(result, equals(json));
+    });
+  });
+}
