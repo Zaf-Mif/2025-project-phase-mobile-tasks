45e182eeec640a5a4d04109f42bd6797d67500b3
Zaferan Miftah <zaferanmiftah@gmail.com>
2025-07-31 18:21:35 +0300

    updating product_model_test with the fixture

--- ecommerce_app/test/features/product/data/model/product_model_test.dart
+++ ecommerce_app/test/features/product/data/model/product_model_test.dart
@@ -1,9 +1,13 @@
+import 'dart:convert';
+
 import 'package:ecommerce_app/features/product/data/models/product_model.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+import '../../../../fixtures/fixture_reader.dart';
+
+
 void main() {
   late ProductModel productModel;
-  late Map<String, dynamic> json;
 
   setUp(() {
     productModel = ProductModel(
@@ -13,25 +17,31 @@ void main() {
       imageUrl: 'test.jpg',
       description: 'Test description',
     );
-
-    json = {
-      'id': 1,
-      'name': 'Test Product',
-      'price': 49.99,
-      'image': 'test.jpg', // Mapped from imageUrl
-      'description': 'Test description',
-    };
   });
 
   group('ProductModel', () {
     test('fromJson should return valid ProductModel', () {
-      final result = ProductModel.fromJson(json);
+      // arrange
+      final Map<String, dynamic> jsonMap = json.decode(fixture('product.json'));
+      // act
+      final result = ProductModel.fromJson(jsonMap);
+      // assert
       expect(result, equals(productModel));
     });
 
     test('toJson should return a valid map', () {
+      // act
       final result = productModel.toJson();
-      expect(result, equals(json));
+      // expected JSON map must match keys exactly like from JSON file
+      final expectedMap = {
+        'id': 1,
+        'name': 'Test Product',
+        'price': 49.99,
+        'image': 'test.jpg', // Note: key is 'image', not 'imageUrl'
+        'description': 'Test description',
+      };
+      // assert
+      expect(result, equals(expectedMap));
     });
   });
 }
